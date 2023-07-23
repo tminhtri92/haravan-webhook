a0303cc502039bae72fbd48da32ecd0fe7b0f071
tminhtri92 <tmtri92.td@gmail.com>
2023-07-23 23:19:27 +0700

    Update

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const haravanValidate = require('haravan-validate')
+
+const haravan = new haravanValidate("4ea2733cecb5402d95ce8a42d7bb72d34a70df62195a251d5f8bde99845aa9af")
+const haravanMiddleware = [haravan, bodyParser.json()]
+
 const app = express()
 const PORT = 3000
 
@@ -11,6 +16,24 @@ app.use(bodyParser.json())
 app.get('/webhook', (req, res) => {
   console.log(req.originalUrl)
   res.status(200).send(req.query?.["hub.challenge"])
+})
+
+app.post('/webhook', middleware, function(req, res) {
+
+  console.log('req.fromHaravan()', req.fromHaravan())
+  // validate the request is from haravan
+  if (!req.fromHaravan()) {
+    return res.status(401).send()
+  }
+
+  // send success notification to haravan
+  // done before to prevent timeout
+  res.status(200).send()
+
+  const topic = req.headers?.['x-haravan-topic']
+
+  console.log('topic',topic)
+
 })
 
 app.post('/webhooks', (req, res) => {
