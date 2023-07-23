24be960f3fc78e8e0690202516e06959e6e217c1
tminhtri92 <tmtri92.td@gmail.com>
2023-07-23 19:54:34 +0700

    Update

--- index.js
+++ index.js
@@ -15,4 +15,9 @@ app.post("/hook", (req, res) => {
   console.log(req.body) // Call your action on the request here
   res.status(200).end() // Responding is important
 })
+
+app.get("/webhooks", (req, res) => {
+  console.log(req) // Call your action on the request here
+  res.status(200).end() // Responding is important
+})
 //...
